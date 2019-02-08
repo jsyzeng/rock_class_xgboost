e313b1a60bcc8fef2dc3fa1c971d9429cf9d908e
jsyzeng <40837433+jsyzeng@users.noreply.github.com>
2019-02-08 08:44:39 -0600

    Add files via upload

---
+++ well_log_plot_utilities.py
@@ -0,0 +1,192 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+#  take a list of colors and facies labels as parameters
+# ------------------------------------------------------------------------
+# Plot 5 well line variables, and Facies (either True or from prediction)
+# Used in raw data analysis or model prediction
+# ------------------------------------------------------------------------
+def make_facies_log_plot(logs, facies_colors):
+    #make sure logs are sorted by depth
+    logs = logs.sort_values(by='Depth')
+    cmap_facies = colors.ListedColormap(
+            facies_colors[0:len(facies_colors)], 'indexed')
+    
+    ztop=logs.Depth.min(); zbot=logs.Depth.max()
+    
+    cluster=np.repeat(np.expand_dims(logs['Facies'].values,1), 100, 1)
+    
+    f, ax = plt.subplots(nrows=1, ncols=6, figsize=(8, 12))
+    ax[0].plot(logs.GR, logs.Depth, '-g')
+    ax[1].plot(logs.ILD_log10, logs.Depth, '-')
+    ax[2].plot(logs.DeltaPHI, logs.Depth, '-', color='0.5')
+    ax[3].plot(logs.PHIND, logs.Depth, '-', color='r')
+    ax[4].plot(logs.PE, logs.Depth, '-', color='black')
+    im=ax[5].imshow(cluster, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+    
+    divider = make_axes_locatable(ax[5])
+    cax = divider.append_axes("right", size="20%", pad=0.05)
+    cbar=plt.colorbar(im, cax=cax)
+    cbar.set_label((17*' ').join([' SS ', 'CSiS', 'FSiS', 
+                                'SiSh', ' MS ', ' WS ', ' D  ', 
+                                ' PS ', ' BS ']))
+    cbar.set_ticks(range(0,1)); cbar.set_ticklabels('')
+    
+    for i in range(len(ax)-1):
+        ax[i].set_ylim(ztop,zbot)
+        ax[i].invert_yaxis()
+        ax[i].grid()
+        ax[i].locator_params(axis='x', nbins=3)
+    
+    ax[0].set_xlabel("GR")
+    ax[0].set_xlim(logs.GR.min(),logs.GR.max())
+    ax[1].set_xlabel("ILD_log10")
+    ax[1].set_xlim(logs.ILD_log10.min(),logs.ILD_log10.max())
+    ax[2].set_xlabel("DeltaPHI")
+    ax[2].set_xlim(logs.DeltaPHI.min(),logs.DeltaPHI.max())
+    ax[3].set_xlabel("PHIND")
+    ax[3].set_xlim(logs.PHIND.min(),logs.PHIND.max())
+    ax[4].set_xlabel("PE")
+    ax[4].set_xlim(logs.PE.min(),logs.PE.max())
+    ax[5].set_xlabel('Facies')
+    
+    ax[1].set_yticklabels([]); ax[2].set_yticklabels([]); ax[3].set_yticklabels([])
+    ax[4].set_yticklabels([]); ax[5].set_yticklabels([])
+    ax[5].set_xticklabels([])
+    f.suptitle('Well: %s'%logs.iloc[0]['Well Name'], fontsize=14,y=0.94)
+
+
+# -------------------------------------------------------------
+# Plot 5 well line variables, true Facies and predicted Facies
+# Used in model validation
+# -------------------------------------------------------------
+def compare_facies_plot(logs, compadre, facies_colors):
+    #make sure logs are sorted by depth
+    logs = logs.sort_values(by='Depth')
+    cmap_facies = colors.ListedColormap(
+            facies_colors[0:len(facies_colors)], 'indexed')
+    
+    ztop=logs.Depth.min(); zbot=logs.Depth.max()
+    
+    cluster1 = np.repeat(np.expand_dims(logs['Facies'].values,1), 100, 1)
+    cluster2 = np.repeat(np.expand_dims(logs[compadre].values,1), 100, 1)
+    
+    f, ax = plt.subplots(nrows=1, ncols=7, figsize=(9, 12))
+    ax[0].plot(logs.GR, logs.Depth, '-g')
+    ax[1].plot(logs.ILD_log10, logs.Depth, '-')
+    ax[2].plot(logs.DeltaPHI, logs.Depth, '-', color='0.5')
+    ax[3].plot(logs.PHIND, logs.Depth, '-', color='r')
+    ax[4].plot(logs.PE, logs.Depth, '-', color='black')
+    im1 = ax[5].imshow(cluster1, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+    im2 = ax[6].imshow(cluster2, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+    
+    divider = make_axes_locatable(ax[6])
+    cax = divider.append_axes("right", size="20%", pad=0.05)
+    cbar=plt.colorbar(im2, cax=cax)
+    cbar.set_label((17*' ').join([' SS ', 'CSiS', 'FSiS', 
+                                'SiSh', ' MS ', ' WS ', ' D  ', 
+                                ' PS ', ' BS ']))
+    cbar.set_ticks(range(0,1)); cbar.set_ticklabels('')
+    
+    for i in range(len(ax)-2):
+        ax[i].set_ylim(ztop,zbot)
+        ax[i].invert_yaxis()
+        ax[i].grid()
+        ax[i].locator_params(axis='x', nbins=3)
+    
+    ax[0].set_xlabel("GR")
+    ax[0].set_xlim(logs.GR.min(),logs.GR.max())
+    ax[1].set_xlabel("ILD_log10")
+    ax[1].set_xlim(logs.ILD_log10.min(),logs.ILD_log10.max())
+    ax[2].set_xlabel("DeltaPHI")
+    ax[2].set_xlim(logs.DeltaPHI.min(),logs.DeltaPHI.max())
+    ax[3].set_xlabel("PHIND")
+    ax[3].set_xlim(logs.PHIND.min(),logs.PHIND.max())
+    ax[4].set_xlabel("PE")
+    ax[4].set_xlim(logs.PE.min(),logs.PE.max())
+    ax[5].set_xlabel('Facies')
+    ax[6].set_xlabel(compadre)
+    
+    ax[1].set_yticklabels([]); ax[2].set_yticklabels([]); ax[3].set_yticklabels([])
+    ax[4].set_yticklabels([]); ax[5].set_yticklabels([])
+    ax[5].set_xticklabels([])
+    ax[6].set_xticklabels([])
+    f.suptitle('Well: %s'%logs.iloc[0]['Well Name'], fontsize=14,y=0.94)
+
+
+# -------------------------------------------------------------
+# Plot 7 well line variables, true Facies, old & new predicted Facies
+# Used in model validation
+# -------------------------------------------------------------
+def compare_facies_plot3(logs, compadre1, compadre2, facies_colors):
+    #make sure logs are sorted by depth
+    logs = logs.sort_values(by='Depth')
+    cmap_facies = colors.ListedColormap(
+            facies_colors[0:len(facies_colors)], 'indexed')
+    
+    ztop=logs.Depth.min(); zbot=logs.Depth.max()
+    
+    cluster1 = np.repeat(np.expand_dims(logs['Facies'].values,1), 100, 1)
+    cluster2 = np.repeat(np.expand_dims(logs[compadre1].values,1), 100, 1)
+    cluster3 = np.repeat(np.expand_dims(logs[compadre2].values,1), 100, 1)
+    
+    f, ax = plt.subplots(nrows=1, ncols=10, figsize=(14, 12))
+    ax[0].plot(logs.GR, logs.Depth, '-g')
+    ax[1].plot(logs.ILD_log10, logs.Depth, '-')
+    ax[2].plot(logs.DeltaPHI, logs.Depth, '-', color='0.5')
+    ax[3].plot(logs.PHIND, logs.Depth, '-', color='r')
+    ax[4].plot(logs.PE, logs.Depth, '-', color='black')
+    ax[5].plot(logs.NM_M, logs.Depth, '-', color='blue')
+    ax[6].plot(logs.RELPOS, logs.Depth, '-', color='pink')
+    im1 = ax[7].imshow(cluster1, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+    im2 = ax[8].imshow(cluster2, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+    im3 = ax[9].imshow(cluster3, interpolation='none', aspect='auto',
+                    cmap=cmap_facies,vmin=1,vmax=9)
+
+    divider = make_axes_locatable(ax[9])
+    cax = divider.append_axes("right", size="10%", pad=0.05)
+    cbar=plt.colorbar(im2, cax=cax)
+    cbar.set_label((17*' ').join([' SS ', 'CSiS', 'FSiS', 
+                                'SiSh', ' MS ', ' WS ', ' D  ', 
+                                ' PS ', ' BS ']))
+    cbar.set_ticks(range(0,1)); cbar.set_ticklabels('')
+    
+    for i in range(len(ax)-3):
+        ax[i].set_ylim(ztop,zbot)
+        ax[i].invert_yaxis()
+        ax[i].grid()
+        ax[i].locator_params(axis='x', nbins=3)
+    
+    ax[0].set_xlabel("GR")
+    ax[0].set_xlim(logs.GR.min(),logs.GR.max())
+    ax[1].set_xlabel("ILD_log10")
+    ax[1].set_xlim(logs.ILD_log10.min(),logs.ILD_log10.max())
+    ax[2].set_xlabel("DeltaPHI")
+    ax[2].set_xlim(logs.DeltaPHI.min(),logs.DeltaPHI.max())
+    ax[3].set_xlabel("PHIND")
+    ax[3].set_xlim(logs.PHIND.min(),logs.PHIND.max())
+    ax[4].set_xlabel("PE")
+    ax[4].set_xlim(logs.PE.min(),logs.PE.max())
+    ax[5].set_xlabel("NM_M")
+    ax[5].set_xlim(logs.NM_M.min()-0.5,logs.NM_M.max()+0.5)
+    ax[6].set_xlabel("RELPOS")
+    ax[6].set_xlim(logs.RELPOS.min(),logs.RELPOS.max())
+    ax[7].set_xlabel('Facies')
+    ax[8].set_xlabel(compadre1)
+    ax[9].set_xlabel(compadre2)
+    
+    ax[1].set_yticklabels([]); ax[2].set_yticklabels([]); ax[3].set_yticklabels([])
+    ax[4].set_yticklabels([]); ax[5].set_yticklabels([]); ax[6].set_yticklabels([])
+    ax[7].set_yticklabels([]); ax[8].set_yticklabels([]); ax[9].set_yticklabels([])
+    ax[7].set_xticklabels([])
+    ax[8].set_xticklabels([])
+    ax[9].set_xticklabels([])
+    f.suptitle('Well: %s'%logs.iloc[0]['Well Name'], fontsize=14,y=0.94)
