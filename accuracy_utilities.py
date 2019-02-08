f0f50bff50fcc4da1b4e705e178317bfaefc1e78
jsyzeng <40837433+jsyzeng@users.noreply.github.com>
2019-02-08 08:33:08 -0600

    Add files via upload

---
+++ accuracy_utilities.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# accuracy is defined as the number of correct classifications divided by the total number of classifications
+def accuracy(conf):
+    total_correct = 0.
+    nb_classes = conf.shape[0]
+    for i in np.arange(0,nb_classes):
+        total_correct += conf[i][i]
+    acc = total_correct/sum(sum(conf))
+    return acc
+
+
+def accuracy_adjacent(conf, adjacent_facies):
+    nb_classes = conf.shape[0]
+    total_correct = 0.
+    for i in np.arange(0,nb_classes):
+        total_correct += conf[i][i]
+        for j in adjacent_facies[i]:
+            total_correct += conf[i][j]
+    return total_correct / sum(sum(conf))
